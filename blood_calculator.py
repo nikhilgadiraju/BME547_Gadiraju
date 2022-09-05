0042c0420e827840e53e739db5a1987e1d2e04cc
Nikhil <nikhil.gadiraju@duke.edu>
2022-09-05 16:03:36 -0400

    Adding Total Cholesterol characterization functions

--- blood_calculator.py
+++ blood_calculator.py
@@ -5,6 +5,7 @@ def interface():
     print("Options:")
     print("1 - Analyze HDL")
     print("2 - Analyze LDL")
+    print("3 - Total Cholesterol Check")
     print("9 - Quit")
     keep_running = True
     while keep_running:
@@ -15,6 +16,8 @@ def interface():
             HDL_driver()
         elif choice == "2":
             LDL_driver()
+        elif choice == "3":
+            totchol_driver()
 
 # Create HDL functions
 def input_HDL():
@@ -23,11 +26,11 @@ def input_HDL():
 
 def check_HDL(param):
     if param >= 60:
-        return "Normal"
+        return "normal"
     elif 40 <= param < 60: # Technically we don't need to specify "<60"
-        return "Borderline Low"
+        return "borderline low"
     else:
-        return "Low"
+        return "low"
 
 def output_HDL_result(hdl_value, charac):
     print("The results for an HDL value of {} is {}".format(hdl_value, charac))
@@ -44,13 +47,13 @@ def input_LDL():
 
 def check_LDL(param):
     if param >= 190:
-        return "Very High"
+        return "very high"
     elif 160 <= param < 190:
-        return "High"
+        return "high"
     elif 130 <= param < 160:
-        return "Borderline High"
+        return "borderline high"
     else:
-        return "Normal"
+        return "normal"
 
 def output_LDL_result(ldl_value, charac):
     print("The results for an LDL value of {} is {}".format(ldl_value, charac))
@@ -59,6 +62,27 @@ def LDL_driver(): # Driver functions essentially call other functions
     ldl_value = input_LDL()
     answer = check_LDL(ldl_value)
     output_LDL_result(ldl_value, answer)
+
+# Create Total Cholesterol Check
+def input_totchol():
+    LDL_input = input("Enter the Total Cholesterol value: ")
+    return int(LDL_input)
+
+def check_totchol(param):
+    if param >= 240:
+        return "high"
+    elif 200 <= param < 240:
+        return "borderline high"
+    else:
+        return "normal"
+
+def output_totchol_result(totchol_value, charac):
+    print("The results for a Total Cholesterol value of {} is {}".format(totchol_value, charac))
+
+def totchol_driver(): # Driver functions essentially call other functions
+    totchol_value = input_totchol()
+    answer = check_totchol(totchol_value)
+    output_totchol_result(totchol_value, answer)
 
 #%% Function Calls
 interface()
